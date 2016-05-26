1b58b19134b0ed9a2ae5a0a728c82631d42a36b4
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-26 17:26:11 -0400

    added functions

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		</header>
 
 		<div>
-			<form action="action_page.php" method="get">
+			<!--<form action="contact.php" method="get">-->
 
 			<header>Name</header>
 			<p>
@@ -114,13 +114,69 @@
 
 
 		<?php
+
+			/*error_log("Before Sending Email");
+			mail("mgoldberg@launchdm.com", "Test e-mail", "Hi, this is a test message!");
+			use command shift r to hard refresh
+			error_log("After Sending Email");*/
+
+			$myemail  = "mgoldberg@launchdm.com";
+			$ryanEmail  = "ryan@launchdm.com";
+
 			$firstName = check_input($_POST['firstName'], "Enter your first name");
 			$middleInitial = check_input($_POST['middleInitial'], "Enter your middle initial");
 			$lastName = check_input($_POST['lastName'], "Enter your last name");
+			$email = check_input($_POST['email'], "Enter your email");
 			$street = check_input($_POST['street'], "Enter your street");
 			$City = check_input($_POST['city'], "Enter your city");
 			$state = check_input($_POST['state'], "Enter your state");
 			$zip = check_input($_POST['zip'], "Enter your zip code");
+
+			if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)){
+    			show_error("E-mail address not valid");
+			}
+
+			function check_input($data, $problem='')
+			{
+   				$data = trim($data);
+    			$data = stripslashes($data);
+   				$data = htmlspecialchars($data);
+   				if ($problem && strlen($data) == 0)
+    			{
+       				show_error($problem);
+    			}
+    			return $data;
+			}
+
+			$message = "Hello!
+
+						Your contact form has been submitted by:
+
+						Name: $yourname
+						E-mail: $email
+
+
+
+						End of message
+						";
+
+			mail($myemail, $subject, $message);
+			mail($ryanEmail, $subject, $message);
+
+			function show_error($myError)
+			{
+			?>
+    			<html>
+    				<body>
+
+   					 <b>Please correct the following error:</b><br />
+   					 <?php echo $myError; ?>
+
+    				</body>
+    			</html>
+			<?php
+			exit();
+			}	
 		?>
 
 	</body>
