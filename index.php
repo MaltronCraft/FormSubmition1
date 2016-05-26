48a2a2a704c84b9fa6b2ba96cf783bd5c5672401
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-26 16:46:39 -0400

    added drop down menu for native state

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 		</header>
 
 		<div>
+			<form action="action_page.php" method="get">
 
 			<header>Name</header>
 			<p>
@@ -25,7 +26,59 @@
 			<p>
 				<b>Street:</b> <input type="text" name="yourname" /><br />
 				<b>City:</b> <input type="text" name="yourname" /><br />
-				<b>State:</b> <br />
+				<b>State:</b> 
+					<select name="State">
+ 					<option value="Alabama">Alabama</option>
+  					<option value="Alaska">Alaska</option>
+  					<option value="Arizona">Arizona</option>
+  					<option value="Arkansas">Arkansas</option>
+  					<option value="California">California</option>
+  					<option value="Colorado">Colorado</option>
+  					<option value="Connecticut">Connecticut</option>
+  					<option value="Delaware">Delaware</option>
+  					<option value="Florida">Florida</option>
+  					<option value="Georgia">Georgia</option>
+  					<option value="Hawaii">Hawaii</option>
+  					<option value="Idaho">Idaho</option>
+  					<option value="Illinois">Illinois</option>
+  					<option value="Iniana">Iniana</option>
+  					<option value="Iowa">Iowa</option>
+  					<option value="Kansas">Kansas</option>
+  					<option value="Kentucky">Kentucky</option>
+  					<option value="Louisiana">Louisiana</option>
+  					<option value="Maine">Maine</option>
+  					<option value="Maryland">Maryland</option>
+  					<option value="Massachusets">Massachusets</option>
+  					<option value="Michigan">Michigan</option>
+  					<option value="Minnesota">Minnesota</option>
+  					<option value="Mississippi">Mississippi</option>
+  					<option value="Missouri">Missouri</option>
+  					<option value="Montana">Montana</option>
+  					<option value="Nebraska">Nebraska</option>
+  					<option value="Nevada">Nevada</option>
+  					<option value="New Hamshire">New Hamshire</option>
+  					<option value="New Jersey">New Jersey</option>
+  					<option value="New Mexico">New Mexico</option>
+  					<option value="New York">New York</option>
+  					<option value="North Carolina">North Carolina</option>
+  					<option value="North Dakota">North Dakota</option>
+  					<option value="Ohio">Ohio</option>
+  					<option value="Oklahoma">Oklahoma</option>
+  					<option value="Oregon">Oregon</option>
+  					<option value="Pennsylvania">Pennsylvania</option>
+  					<option value="Rhode Island">Rhode Island</option>
+  					<option value="South Carolina">South Carolina</option>
+  					<option value="South Dakota">South Dakota</option>
+  					<option value="Tennessee">Tennessee</option>
+  					<option value="Texas">Texas</option>
+  					<option value="Utah">Utah</option>
+  					<option value="Vermont">Vermont</option>
+  					<option value="Virginia">Virginia</option>
+  					<option value="Washington">Washington</option>
+  					<option value="West Virginia">West Virginia</option>
+  					<option value="Wisconsin">Wisconsin</option>
+  					<option value="Wyoming">Wyoming</option>
+					</select><br />
 				<b>Zip Code:</b> <input type="text" name="yourname" /><br />
 			</p>	
 
@@ -52,6 +105,10 @@
 
 
 			<p>Additional Information: <input type="text" name="yourname" /><br /></p>
+
+			<input type="submit" value="Submit">
+
+			</form>
 
 		</div>
 
