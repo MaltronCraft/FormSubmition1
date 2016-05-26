ce2bbb441ec3ec512333b4c17869fed89c774ded
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-26 16:07:58 -0400

    set up basic index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+	<head>
+		<title>Submition Form</title>
+	</head>
+
+	<body>
+
+		<header>
+			Please Fill Out Form
+		</header>
+
+		<div>
+
+			
+
+		</div>
+
+	</body>
+
+</HTML>
