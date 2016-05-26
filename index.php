b23e639c9ff8f929a81cc312662e51a779d65113
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-26 16:25:56 -0400

    set up input type things

--- index.php
+++ index.php
@@ -12,7 +12,46 @@
 
 		<div>
 
-			
+			<header>Name</header>
+			<p>
+				<b>First Name:</b> <input type="text" name="yourname" /><br />
+				<b>Middle Initial:</b> <input type="text" name="yourname" /><br />
+				<b>Last Name:</b> <input type="text" name="yourname" /><br />
+				<b>Email:</b> <input type="text" name="yourname" /><br />
+				<b>Phone Number:</b> <input type="text" name="yourname" /><br />
+			</p>
+
+			<header>Address</header>
+			<p>
+				<b>Street:</b> <input type="text" name="yourname" /><br />
+				<b>City:</b> <input type="text" name="yourname" /><br />
+				<b>State:</b> <br />
+				<b>Zip Code:</b> <input type="text" name="yourname" /><br />
+			</p>	
+
+			<header>Department</header>
+			<p>
+				<input type="radio" name="gender" value="male" checked> Development<br>
+				<input type="radio" name="gender" value="male" checked> Sales<br>
+				<input type="radio" name="gender" value="male" checked> Project Management<br>
+				<input type="radio" name="gender" value="male" checked> Design<br>
+				<input type="radio" name="gender" value="male" checked> Marketing<br>
+				<input type="radio" name="gender" value="male" checked> SalesBuddy<br>
+			</p>
+
+			<header>Areas of Intrest</header>
+			<p>	
+				<input type="checkbox" name="Intrest" value="Reading">I like to read.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like to draw.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like to play video games.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like computers.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like sports.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like cars.<br>
+				<input type="checkbox" name="Intrest" value="Reading">I like politics.<br>
+			</p>
+
+
+			<p>Additional Information: <input type="text" name="yourname" /><br /></p>
 
 		</div>
 
